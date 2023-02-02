340cb60c9c57a27f4c778fe019281a98674b6877
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2023-02-02 12:51:08 -0500

    Fixed social icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,12 +6,11 @@ export const Header = () => {
         <header className='header'>
             <div className="container">
                 <div className="links">
-                    <ul className="social-links">
-                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in">Linkedin</a></li>
+                    <ul className="gl">
+                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in"><img src="/img/in.png" alt="Linkedin" /></a></li>
+                        <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/hub.png" alt="Github" /></a></li>
                     </ul>
                     <ul className="nav">
-                    <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/github.png" alt="Github" /></a></li>
-
                         <li><a href="#experience">Experience</a></li>
                         <li><a href="#examples">Designs</a> </li>
                         <li><a href="#about">About</a> </li>
