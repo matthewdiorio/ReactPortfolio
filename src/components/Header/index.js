e89a3c93ec8fb3e6c38e94ca026c1b63dd048b9d
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2023-02-02 13:17:05 -0500

    Updated header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,14 +6,12 @@ export const Header = () => {
         <header className='header'>
             <div className="container">
                 <div className="links">
-                    <ul className="gl">
-                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in"><img src="/img/in.png" alt="Linkedin" /></a></li>
-                        <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/hub.png" alt="Github" /></a></li>
-                    </ul>
                     <ul className="nav">
+                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in"><img src="/img/in.png" alt="Linkedin" /></a></li>
                         <li><a href="#experience">Experience</a></li>
                         <li><a href="#examples">Designs</a> </li>
                         <li><a href="#about">About</a> </li>
+                        <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/hub.png" alt="Github" /></a></li>
                     </ul>
                 </div>
             </div>
