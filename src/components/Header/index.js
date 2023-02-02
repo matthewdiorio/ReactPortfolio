c9d13279169c5c104d16fc139eec4b6815043fa5
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2023-02-02 12:27:33 -0500

    Update index.js
    test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ export const Header = () => {
             <div className="container">
                 <div className="links">
                     <ul className="social-links">
-                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in"><img src="/img/linkedin.png" alt="Linkedin" /></a></li>
+                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in">Linkedin</a></li>
                         <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/github.png" alt="Github" /></a></li>
                     </ul>
                     <ul className="nav">
