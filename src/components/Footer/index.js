0434db02a0faaae08c9ebb3a1ebd3a771426610d
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2023-02-02 12:52:16 -0500

    fixed social links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,9 +6,9 @@ export const Footer = () => {
         <footer className='footer'>
             <div className="container">
                 <div className="links">
-                    <ul className="social-links">
-                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in"><img src="/img/linkedin.png" alt="Linkedin" /></a></li>
-                        <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/github.png" alt="Github" /></a></li>
+                    <ul className="gl">
+                        <li><a href="https://www.linkedin.com/in/matthewdiorio" aria-label="linked-in"><img src="/img/in.png" alt="Linkedin" /></a></li>
+                        <li><a href="https://www.github.com/matthewdiorio" aria-label="Github"><img src="/img/hub.png" alt="Github" /></a></li>
                     </ul>
                     <div class="email"><a href="mailto:matthewdiorio@yahoo.com" className="email-link">matthewdiorio@yahoo.com</a></div>
                 </div>
